46d1fe50ec1cd4a313e202bd6766a082a91c8b06
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-07-16 15:32:36 +0200

    first version

---
+++ readme.md
@@ -0,0 +1,1 @@
+This plugin adds routes to the REST API which can be used by a TYPO3 extension (or other external services as well).
---
+++ routes/InstituteMap.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * InstiuteMap - TYPO3 routes for institute related stuff
+ *
+ * @author Thomas Hackl <thomas.hackl@uni-passau.de>
+ */
+
+class InstituteMap extends RESTAPI\RouteMap {
+
+    /**
+     * Returns the institute hierarchy.
+     *
+     * @get /typo3/institutes
+     */
+    public function getInstituteHierarchy() {
+        $institutes = array();
+        // Get faculties.
+        $faculties = Institute::findBySQL("`Institut_id`=`fakultaets_id` ORDER BY `Name`");
+        foreach ($faculties as $faculty) {
+            $data = array(
+                'id' => $faculty->id,
+                'name' => $faculty->name
+            );
+            $children = Institute::findByFaculty($faculty->id);
+            if ($children) {
+                foreach ($children as $c) {
+                    $data['children'][] = array(
+                        'id' => $c->id,
+                        'name' => $c->name
+                    );
+                }
+            }
+            $institutes[] = $data;
+        }
+        return $institutes;
+    }
+
+    /**
+     * Returns the range tree hierarchy.
+     *
+     * @get /typo3/rangetree
+     */
+    public function getRangeTree() {
+        $tree = TreeAbstract::getInstance('StudipRangeTree', array('visible_only' => 1));
+        return self::buildRangeTreeLevel('root', $tree);
+    }
+
+    /**
+     * Recursively builds the tree structure of the range hierarchy.
+     *
+     * @param  String          $parent_id current level
+     * @param  StudipRangeTree $tree      range tree object
+     * @return array The tree structure of institutes and pseudo levels.
+     */
+    private function buildRangeTreeLevel($parent_id, &$tree) {
+        $data = array();
+        foreach ($tree->getKids($parent_id) as $kid) {
+            $i = Institute::find($kid);
+            $data[] = array(
+                'id' => $i->id,
+                'name' => $i->name
+            );
+            $data['children'] = self::buildRangeTreeLevel($kid, $tree);
+        }
+        return $data;
+    }
+
+}
+    
