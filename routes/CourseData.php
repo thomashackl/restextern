66bf36196cb1f70f6c44a8ed562e6ca73e6fa5b2
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-07-31 14:25:26 +0200

    course types

---
+++ routes/CourseData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RESTAPI\Routes;
+
+/**
+ * CourseData - TYPO3 routes for course related data, like available course
+ * types.
+ *
+ * @author Thomas Hackl <thomas.hackl@uni-passau.de>
+ */
+
+class CourseData extends \RESTAPI\RouteMap {
+
+    /**
+     * Returns avaible course types.
+     *
+     * @get /typo3/coursetypes/:institute
+     * @get /typo3/coursetypes
+     */
+    public function getCourseTypes($institute) {
+        $types = array();
+        if ($institute) {
+            $types = DBManager::get()->fetchAll(
+                "SELECT DISTINCT t.`id`, t.`name`, c.`name`
+                FROM `sem_types` t
+                    INNER JOIN `sem_classes` c ON (t.`class`=c.`id`)
+                    INNER JOIN `seminare` s ON (s.`status`=t.`id`)
+                    INNER JOIN `seminar_inst` si ON (s.`Seminar_id`=si.`seminar_id`)
+                WHERE si.`institut_id`='f05'
+                ORDER BY c.`id`, t.`name`");
+        } else {
+            $types = DBManager::get()->fetchAll(
+                "SELECT DISTINCT t.`id`, t.`name` AS type, c.`name` AS classname
+                FROM `sem_types` t
+                    INNER JOIN `sem_classes` c ON (t.`class`=c.`id`)
+                ORDER BY c.`id`, t.`name`");
+        }
+        return $types;
+    }
+
+
+}
