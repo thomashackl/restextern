1ce259e2b382b9acd34a704818ec6140eaa051d4
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-08-04 14:45:42 +0200

    statusgroups at institute

--- routes/InstituteHierarchy.php
+++ routes/InstituteHierarchy.php
@@ -51,6 +51,23 @@ class InstituteHierarchy extends \RESTAPI\RouteMap {
     }
 
     /**
+     * Returns all statusgroup names found at the given institute
+     * (and sublevels if configured). The statusgroup ID is not considered.
+     *
+     * @get /typo3/statusgroupnames/:institute/:aggregate
+     * @get /typo3/statusgroupnames/:institute
+     */
+    public function getStatusgroupNames($institute, $aggregate=false) {
+        if ($aggregate) {
+            $ids = DBManager::get()->fetchAll("SELECT `Institut_id` FROM `Institute` WHERE `fakultaets_id`=?", array($institute));
+        } else {
+            $ids = array($institute);
+        }
+        $groups = DBManager::get()->fetchAll("SELECT DISTINCT `statusgruppe_id`, `name` FROM `statusgruppen` WHERE `range_id` IN (?) ORDER BY `name`", array($ids));
+        return array_keys(self::getStatusgroupChildren($ids, true));
+    }
+
+    /**
      * Recursively builds the tree structure of the range hierarchy.
      *
      * @param  String          $parent_id current level
@@ -72,6 +89,30 @@ class InstituteHierarchy extends \RESTAPI\RouteMap {
             }
         }
         return $level;
+    }
+
+    /**
+     * Recursively finds all statusgroups (indirectly) belonging to the given
+     * set of IDs.
+     *
+     * @param String $ids       parent IDs to check
+     * @param bool   $name_only are only the group names required?
+     * @return array the groups (or just their names) belonging to the given IDs
+     */
+    private function getStatusgroupChildren($ids, $name_only=false) {
+        $result = array();
+        $children = DBManager::get()->fetchAll("SELECT `statusgruppe_id`, `name`, `range_id` FROM `statusgruppen` WHERE `range_id` IN (?) ORDER BY `name`", array($ids));
+        if ($children) {
+            foreach ($children as $child) {
+                if ($name_only) {
+                    $result[$child['name']] = true;
+                }
+            }
+            $result = array_merge($result, self::getStatusgroupChildren(array_map(function($e){
+                return $e['statusgruppe_id'];
+            }, $children), $name_only));
+        }
+        return $result;
     }
 
 }
