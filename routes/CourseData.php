a5e280460fa109cbed070f7082b68ef44cab1cdf
Thomas Hackl <thomas.hackl@uni-passau.de>
2015-09-02 13:43:56 +0200

    extended course search for frontend

--- routes/CourseData.php
+++ routes/CourseData.php
@@ -30,12 +30,14 @@ class CourseData extends \RESTAPI\RouteMap {
                     INNER JOIN `seminare` s ON (s.`status`=t.`id`)
                     INNER JOIN `seminar_inst` si ON (s.`Seminar_id`=si.`seminar_id`)
                 WHERE si.`institut_id`=?
+                    AND c.`studygroup_mode` != 1
                 ORDER BY c.`id`, t.`name`", array($institute));
         } else {
             $types = DBManager::get()->fetchAll(
                 "SELECT DISTINCT t.`id`, t.`name` AS type, c.`name` AS classname, c.`id` AS typeclass
                 FROM `sem_types` t
                     INNER JOIN `sem_classes` c ON (t.`class`=c.`id`)
+                WHERE c.`studygroup_mode` != 1
                 ORDER BY c.`id`, t.`name`");
         }
         return $types;
@@ -71,8 +73,8 @@ class CourseData extends \RESTAPI\RouteMap {
      * Finds courses matching the given search term. The search range can be
      * restricted to a given semester.
      *
-     * @get /typo3/coursesearch/:searchterm
      * @get /typo3/coursesearch/:searchterm/:semester_id
+     * @get /typo3/coursesearch/:searchterm
      */
     public function searchCourses($searchterm, $semester_id='')
     {
@@ -95,6 +97,78 @@ class CourseData extends \RESTAPI\RouteMap {
         }
         $query .= " ORDER BY s.`start_time` DESC, number, name";
         return DBManager::get()->fetchAll($query, $parameters);
+    }
+
+    /**
+     * Finds courses matching the given search term. The search range can be
+     * restricted to a given semester, an institute or a course type.
+     * More data is returned than in searchCourses.
+     *
+     * @get /typo3/extendedcoursesearch/:searchterm/:semester_id/:institute_id/:coursetype
+     * @get /typo3/extendedcoursesearch/:searchterm/:semester_id/:institute_id
+     * @get /typo3/extendedcoursesearch/:searchterm/:semester_id
+     * @get /typo3/extendedcoursesearch/:searchterm
+     */
+    public function extendedCourseSearch($searchterm, $semester_id='', $institute_id='', $coursetype='')
+    {
+        $select = "SELECT DISTINCT s.*";
+        $from = " FROM `seminare` s
+            ";
+        $where = " WHERE (s.`VeranstaltungsNummer` LIKE :searchterm
+                OR s.`Name` LIKE :searchterm
+                OR s.`Untertitel` LIKE :searchterm)
+            AND s.`visible` = 1
+            AND s.`status` NOT IN (:excludetypes)";
+        $parameters = array(
+            'searchterm' => '%'.utf8_decode(urldecode($searchterm)).'%',
+            'excludetypes' => studygroup_sem_types() ?: array()
+        );
+        if ($semester_id) {
+            $from .= " JOIN `semester_data` sd ON (
+                (s.`duration_time` != -1 AND s.`start_time` + s.`duration_time` BETWEEN sd.`beginn` AND sd.`ende`)
+                OR
+                (s.`duration_time` = -1 AND s.`start_time` <= sd.`ende`))";
+            $where .= " AND sd.`semester_id`=:semester";
+            $parameters['semester'] = $semester_id;
+            $order = " ORDER BY s.`VeranstaltungsNummer`, s.`Name`";
+        } else {
+            $order = " ORDER BY s.`start_time` DESC, s.`VeranstaltungsNummer`, s.`Name`";
+        }
+        if ($institute_id) {
+            $from .= " JOIN `seminar_inst` si ON (s.`Seminar_id`=si.`seminar_id`)";
+            $where .= " AND si.`institut_id`=:institute";
+            $parameters['institute'] = $institute_id;
+        }
+        if ($coursetype) {
+            $from .= " JOIN `sem_types` t ON (s.`status`=t.`id`)";
+            $where .= " AND s.`status`=:coursetype";
+            $parameters['coursetype'] = $coursetype;
+        }
+        $query = $select.$from.$where.$order;
+        $log = fopen('/Applications/MAMP/tmp/php/tx.log', 'w');
+        $data = DBManager::get()->fetchAll($query, $parameters, 'Course::buildExisting');
+
+        $courses = array();
+        foreach ($data as $c) {
+            $type = $c->getSemType();
+            $course = array(
+                'id' => $c->id,
+                'number' => $c->veranstaltungsnummer,
+                'name' => $c->name,
+                'subtitle' => $c->untertitel,
+                'type' => $GLOBALS['SEM_TYPE'][$c->status]['name']
+            );
+            foreach (\SimpleORMapCollection::createFromArray($c->getMembersWithStatus('dozent'))->orderBy('position') as $l) {
+                $course['lecturers'][] = array(
+                    'id' => $l->id,
+                    'firstname' => $l->vorname,
+                    'lastname' => $l->nachname,
+                    'username' => $l->username
+                );
+            }
+            $courses[] = $course;
+        }
+        return $courses;
     }
 
     /**
